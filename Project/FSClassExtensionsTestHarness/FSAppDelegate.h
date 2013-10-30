82deec543167c2cf78c6fd39fe9316b5f9e6bc82
Bennett Smith <bennett@focalshift.com>
2013-10-29 20:14:29 -0700

    Creates pod for the project.

---
+++ Project/FSClassExtensionsTestHarness/FSAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  FSAppDelegate.h
+//  FSClassExtensionsTestHarness
+//
+//  Created by Bennett Smith on 10/29/13.
+//  Copyright (c) 2013 FocalShift, LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FSAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Project/FSClassExtensionsTestHarness/FSViewController.h
@@ -0,0 +1,13 @@
+//
+//  FSViewController.h
+//  FSClassExtensionsTestHarness
+//
+//  Created by Bennett Smith on 10/29/13.
+//  Copyright (c) 2013 FocalShift, LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FSViewController : UIViewController
+
+@end
